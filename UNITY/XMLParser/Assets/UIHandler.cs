6d514e698ad312f8ecb2d1ed272f8e2a4497542d
Jort Driegen <39172426+jortdr@users.noreply.github.com>
2020-11-17 16:12:52 +0100

    Added Unity Version + Sorted Files in two sep. folders

---
+++ UNITY/XMLParser/Assets/UIHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIHandler : MonoBehaviour
+{
+    [SerializeField] private Image[] ColorFields;
+    [SerializeField] private TMP_Text[] nameFields;
+
+    private Color32 Red = new Color32(255,93,93,255);
+    private Color32 Green = new Color32(93,255,93,255);
+
+    public void DisplayData(List<string> access, List<string> color, List<string> name)
+    {
+        //Check access status
+
+        for (int i = 0; i < ColorFields.Length; i++)
+        {
+            switch (color[i])
+            {
+                case "red":
+                    ColorFields[i].color = Red;
+                    break;
+                case "green":
+                    ColorFields[i].color = Green;
+                    break;
+            }
+
+            switch (access[i])
+            {
+                case "ok":
+                    break;
+
+                default:
+                    ColorFields[i].color = Color.gray;
+                    break;
+            }
+
+            //Set name field to correct i
+            nameFields[i].text = name[i];
+        }
+    }
+}
---
+++ UNITY/XMLParser/Assets/XMLParser.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+
+public class XMLParser : MonoBehaviour
+{
+    private XmlTextReader reader = new XmlTextReader("http://lodewijk.edgetech.nl:3343/monitor");
+    private UIHandler UI;
+
+
+    public List<string> accessesValue = new List<string>();
+
+    public List<string> colorsValue = new List<string>();
+
+    public List<string> nameValue = new List<string>();
+
+    private void Start()
+    {
+        UI = GetComponent<UIHandler>();
+        LoadFromURL();
+
+    }
+
+
+    void LoadFromURL()
+    {
+
+
+        //Inspect nodes
+        while (reader.Read())
+        {
+            while (reader.MoveToNextAttribute())
+            {
+                // Read the attributes.
+                Debug.Log("NAME: " + reader.Name + " | VALUE: " + reader.Value);
+
+                switch (reader.Name)
+                {
+                    case "access":
+                        accessesValue.Add(reader.Value);
+                        break;
+
+                    case "color":
+                        colorsValue.Add(reader.Value);
+                        break;
+
+                    case "name":
+                        nameValue.Add(reader.Value);
+                        break;
+                }
+            }
+        }
+
+        UI.DisplayData(accessesValue, colorsValue, nameValue);
+
+    }
+}
+
+
